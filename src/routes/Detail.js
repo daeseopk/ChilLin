a701437b12afe2bfbbfde784975e6a684b96004f
daeseopk <eotjqdlee@gmail.com>
2022-03-27 18:20:35 +0900

    search page dev completed

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,27 +2,26 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import MovieDetail from "../components/MovieDetail";
 
-function Detail(){
-  const [movie, setMovie] = useState();
-  const [loading, setLoading] = useState(true);
-  const {id} = useParams();
+// TODO Detail 페이지 구현하기
+function Detail() {
+   const [movie, setMovie] = useState();
+   const [loading, setLoading] = useState(true);
+   const { id } = useParams();
 
-  const getMovie = async()=>{
-    const response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`);
-    const json = await response.json();
-    setMovie(json.data.movie);
-    setLoading(false);
-  }
-  useEffect(()=>{
-    getMovie();
-  },[])
+   const getMovie = async () => {
+      const response = await fetch(
+         `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      );
+      const json = await response.json();
+      setMovie(json.data.movie);
+      setLoading(false);
+   };
+   useEffect(() => {
+      getMovie();
+   }, []);
 
-  return(
-    <div>
-      {loading ? <h1>loading...</h1> : 
-      <MovieDetail movie={movie}/>
-      }
-    </div>
-  );
+   return (
+      <div>{loading ? <h1>loading...</h1> : <MovieDetail movie={movie} />}</div>
+   );
 }
 export default Detail;
